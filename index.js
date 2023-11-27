99416eb4492343c70feaf333e8a3ffb20bc0b508
EduardoMatos08 <matos08edu@gmail.com>
2023-11-27 14:16:15 -0300

    Projeto de pedidos de hamburgueria finalizado

--- index.js
+++ index.js
@@ -3,8 +3,7 @@ const express = require('express')
 const uuid = require('uuid')
 
 // Definino a Porta
-const port = 1834
-console.log(`Server Hosted on Port: ${port}.`)
+const port = 1835
 const app = express()
 app.use(express.json())
 
@@ -16,3 +15,45 @@ app.get('/orders', (request, response) => {
     return response.status(200).json(orders)
 })
 
+// Enviando Pedido - ".post"
+app.post('/orders', (request, response) => {
+    const { name, client_order, price, status } = request.body
+    const order = { id: uuid.v4(), name, client_order, price, status }
+
+    orders.push(order)
+
+    return response.status(201).json(order)
+})
+
+// Atualizando Pedido para: "Pronto" - ".put / .patch"
+app.put('/orders/:id', (request, response) => {
+    const { id } = request.params
+    const { name, client_order, price, status } = request.body
+
+    const updatedOrder = { id, name, client_order, price, status }
+    const index = orders.findIndex(order => order.id === id)
+
+    if (index >= 0) {
+        orders[index] = updatedOrder
+    } else if (index === -1) {
+        return response.status(404).json({ mensage: "Order not Found" })
+    } else {
+        return response.status(500).json({ mensage: "Internal Server Error" })
+    }
+
+    return response.status(201).json(updatedOrder)
+})
+
+// Deletando Pedido - ".delete"
+app.delete('/orders/:id', (request, response) => {
+    const { id } = request.params
+    const index = orders.findIndex(order => order.id === id)
+
+    orders.splice(index, 1)
+    return response.status(404).json({ mensage: "Order not Found" })
+})
+
+// console.log() da porta
+app.listen(port, () => {
+    console.log(`Server hosted on port: ${port}.`)
+})
