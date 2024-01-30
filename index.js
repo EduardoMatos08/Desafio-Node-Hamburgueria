bf36cf529a941b1bdf2b98312a44c6fe8e536301
EduardoMatos08 <matos08edu@gmail.com>
2024-01-30 15:38:21 -0300

    Try & Catch utilizados para fazer o tratamento de erros - 30/01/2024

--- index.js
+++ index.js
@@ -27,41 +27,59 @@ const checkOrderId = (request, response, next) => {
 
 }
 
+// Tratamento de Erros - TRY && CATCH && FINALLY
+
 // Buscando Pedidos - ".get"
 app.get('/orders', (request, response) => {
-    return response.status(200).json(orders)
+    try{
+        return response.status(200).json(orders)
+    } catch{
+        return response.status(500).json({error:err.message})
+    }
 })
 
 // Enviando Pedido - ".post"
 app.post('/orders', (request, response) => {
-    const { name, client_order, price, status } = request.body
-    const order = { id: uuid.v4(), name, client_order, price, status }
-
-    orders.push(order)
-
-    return response.status(201).json(order)
+    try {
+        const { name, client_order, price, status } = request.body
+        const order = { id: uuid.v4(), name, client_order, price, status }
+    
+        orders.push(order)
+    
+        return response.status(201).json(order)
+    } catch(err){
+        return response.status(500).json({error:err.message})
+    }
 })
 
 // Atualizando Pedido para: "Pronto" - ".put / .patch"
 app.put('/orders/:id', checkOrderId, (request, response) => {
-    const { id } = request.params
-    const { name, client_order, price, status } = request.body
-
-    const updatedOrder = { id, name, client_order, price, status }
-    const index = orders.findIndex(order => order.id === id)
-
-    orders[index] = updatedOrder
-
-    return response.status(201).json(updatedOrder)
+    try {
+        const { id } = request.params
+        const { name, client_order, price, status } = request.body
+    
+        const updatedOrder = { id, name, client_order, price, status }
+        const index = orders.findIndex(order => order.id === id)
+    
+        orders[index] = updatedOrder
+    
+        return response.status(201).json(updatedOrder)
+    } catch(err){
+        return response.status(500).json({error:err.message})
+    }
 })
 
 // Deletando Pedido - ".delete"
 app.delete('/orders/:id', checkOrderId, (request, response) => {
-    const { id } = request.params
-    const index = orders.findIndex(order => order.id === id)
+    try{
+        const { id } = request.params
+        const index = orders.findIndex(order => order.id === id)
 
-    orders.splice(index, 1)
-    return response.status(404).json({ mensage: "Order not Found" })
+        orders.splice(index, 1)
+        return response.status(404).json({ mensage: "Order not Found" })
+    } catch(err){
+        return response.status(500).json({error:err.message})
+    }
 })
 
 // console.log() da porta
